9a4c95705562db1ad4e1dea67d5ec5f96e123369
Surya Kant <suryakantasahoo533@gmail.com>
2021-06-05 04:50:51 +0530

    Changed weather description format

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat,lon,callback)=>{
             
             callback('Unable to find location!',undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.cloudcover + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.cloudcover + '% chance of rain.' + ' Humidity is ' + body.current.humidity + '%.')
         }
     })
 
