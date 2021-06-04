15423ac122e5574fdfdf04175f36a9bc20d65ba2
Surya Kant <suryakantasahoo533@gmail.com>
2021-06-05 04:37:35 +0530

    Setup app for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit',(e)=>{
     msgOne.textContent = 'Loading....'
     msgTwo.textContent = ''
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
     
     
     
